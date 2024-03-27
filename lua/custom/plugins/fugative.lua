0d0e4bc083f7b1a682513000eb8fb0632013986e
Marley Powell <55280588+marleypowell@users.noreply.github.com>
2024-03-27 16:19:45 +0000

    updated fugative config

--- lua/custom/plugins/fugative.lua
+++ lua/custom/plugins/fugative.lua
@@ -1,3 +1,13 @@
 return {
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { desc = '[G]it' })
+      vim.keymap.set('n', '<leader>gs', function()
+        vim.cmd.Git 'status'
+      end, { desc = '[G]it [S]tatus' })
+    end,
+  },
+  'tpope/vim-rhubarb',
+  'cedarbaum/fugitive-azure-devops.vim',
 }
